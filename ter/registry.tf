a60d70199a8824b20f8ea6c2496cbe850d262aaa
SemikovaTV <129278233+SemikovaTV@users.noreply.github.com>
2024-08-29 19:59:43 +0300

    Delete ter/registry.tf

--- ter/registry.tf
+++
@@ -1,9 +0,0 @@
-resource "yandex_container_registry" "registry" {
-  name = "docker-registry"
-  folder_id = var.folder_id
-}
-resource "yandex_container_registry_ip_permission" "my_ip_permission" {
-  registry_id = yandex_container_registry.registry.id
-  push = ["0.0.0.0/24", "89.169.174.252/24"]
-  pull = ["0.0.0.0/24"]
-}
